99db3ef261923ed446c51f71fa313a8a6a68ddd2
Neha Joshi <neha@njoncode.local>
2021-02-08 13:25:27 +0530

    (Project) Popular Nav Bar

--- app/index.js
+++ app/index.js
@@ -1,31 +1,15 @@
-/*
-Creating a list of <li> elements from the friends array being passed into List. Your final UI for each one should look like this.
-solution */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import Popular from './components/popular';
 
-class List extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <ul id="friends">
-        {this.props.friends.map((friend) => (
-          <li key={friend.name}>{friend.name}</li>
-        ))}
-      </ul>
+      <div className="container">
+        <Popular />
+      </div>
     );
   }
 }
-
-ReactDOM.render(
-  <List
-    friends={[
-      { id: 893, name: 'Mikenzi' },
-      { id: 871, name: 'Cash' },
-      { id: 982, name: 'Steven' },
-      { id: 961, name: 'Kimmy' },
-      { id: 117, name: 'Doug' },
-    ]}
-  />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
