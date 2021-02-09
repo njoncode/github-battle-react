1390ff65216f286f55740d49061bab5d598fe60f
Neha Joshi <neha@njoncode.local>
2021-02-09 20:13:39 +0530

    (Project) Navbar State

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+    });
+  }
+
   render() {
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
       <ul className="flex-center">
         {languages.map((language) => (
           <li key={language}>
-            <button onClick={() => this.updateLanguage(language)}>
+            <button
+              className="btn-clear nav-link"
+              style={
+                language === this.state.selectedLanguage
+                  ? { color: 'rgb(187, 46, 31)' }
+                  : null
+              }
+              onClick={() => this.updateLanguage(language)}
+            >
               {language}
             </button>
           </li>
--- app/index.js
+++ app/index.js
@@ -5,40 +5,8 @@ import Popular from './components/popular';
 
 class App extends React.Component {
   render() {
-    // bind, call and apply basics
-    function greet(lang1, lang2) {
-      console.log(`My name is ${this.name} & I know ${lang1}, ${lang2}.`);
-    }
-
-    const user = {
-      name: 'Sam',
-      age: 20,
-    };
-
-    const languages = ['Java', 'React'];
-
-    // .call
-
-    const greetConst = greet.call(user, languages[0], languages[1]);
-
-    // .apply
-
-    greet.apply(user, languages);
-
-    // .bind
-
-    const fn = greet.bind(user, languages[0], languages[1]);
-    fn();
-    // fn is a function
-
-    // OR
-
-    const greetings = greet.bind(user, languages[0], languages[1]);
-    greetings();
-    // greetings is a function
-
     return (
-      <div>
+      <div className="container">
         <Popular />
       </div>
     );
