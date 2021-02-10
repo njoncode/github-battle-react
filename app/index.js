3e50cf0ebaf3bc95221e5e624f8c3ba469d253f5
Neha Joshi <neha@njoncode.local>
2021-02-10 13:13:30 +0530

    (Project) Fetch Repos

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Popular from './components/popular';
+import Popular from './components/Popular';
+import Battle from './components/Battle';
 
 class App extends React.Component {
   render() {
